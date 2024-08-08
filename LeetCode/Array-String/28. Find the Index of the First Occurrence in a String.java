8bfa45f7223efaa593b6b3458c3012ee22422dbe
GiannisTsevrenis <ts3v@outlook.com>
2024-08-08 20:15:44 +0300

    Update 28. Find the Index of the First Occurrence in a String.java

--- LeetCode/Array-String/28. Find the Index of the First Occurrence in a String.java
+++ LeetCode/Array-String/28. Find the Index of the First Occurrence in a String.java
@@ -12,3 +12,22 @@ public class FirstOccurrenceInAString {
         }
     }
 }
+// import java.util.regex.Pattern;
+// import java.util.regex.Matcher;
+
+// public class FirstOccurrenceInAString {
+//     public static void main(String[] args) {
+//         System.out.println(new FirstOccurrenceInAString().strStr("sadbutsad", "sad"));
+//         System.out.println(new FirstOccurrenceInAString().strStr("leetcode", "leeto"));
+//     }
+
+//     public int strStr(String haystack, String needle) {
+//         Pattern pattern = Pattern.compile(needle);
+//         Matcher matcher = pattern.matcher(haystack);
+//         if (matcher.find()) {
+//             return matcher.start();
+//         } else {
+//             return -1;
+//         }
+//     }
+// }
