bfe85de257a39b177e14a27b470ef376e372ee47
GiannisTsevrenis <ts3v@outlook.com>
2024-09-13 09:29:24 +0300

    Update 1310. XOR Queries of a Subarray.java

--- LeetCode/BitManipulation/1310. XOR Queries of a Subarray.java
+++ LeetCode/BitManipulation/1310. XOR Queries of a Subarray.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int[] result = new int[queries.length];
+        int [] prefix = new int[arr.length+1];
+        prefix[0] = 0;
+        for (int i = 0; i < arr.length; i++) {
+            prefix[i+1] = arr[i]^prefix[i];
+        }
+        int[] result = new  int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int add =arr[queries[i][0]];
-            int j =queries[i][0]+1;
-            while (j <=queries[i][1] ) {
-                add ^= arr[j];
-                j++;
-            }
-
-            result[i] = add;
+            result[i] = prefix[queries[i][1]+1]^prefix[queries[i][0]];
         }
         return result;
     }
-
 }
