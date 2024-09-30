9c239de97115cdf3935fa2709a9d8746bb33faf6
GiannisTsevrenis <ts3v@outlook.com>
2024-09-30 11:39:17 +0300

    Update 21. Merge Two Sorted Lists.java

--- LeetCode/Recursion/21. Merge Two Sorted Lists.java
+++ LeetCode/Recursion/21. Merge Two Sorted Lists.java
@@ -1,26 +1,28 @@
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null && list2 == null) {
-            return list1;
-        }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode result = new ListNode();
         ListNode current = result;
-        ListNode p1 = list1;
-        ListNode p2 = list2;
-        while (p1 != null || p2 != null) {
-            if (p1 == null) {
-                current.next = p2;
-                p2 = p2.next;
-            } else if (p2 == null) {
-                current.next = p1;
-                p1 = p1.next;
-            } else if (p1.val <= p2.val) {
-                current.next = p1;
-                p1 = p1.next;
+        while (list1 != null && list2 != null) {
+            if (list1.val >= list2.val) {
+                current.next = list2;
+                list2 = list2.next;
             } else {
-                current.next = p2;
-                p2 = p2.next;
+                current.next = list1;
+                list1 = list1.next;
             }
             current = current.next;
         }
+        current.next = (list1 == null)? list2: list1;
         return result.next;
     }
+}
