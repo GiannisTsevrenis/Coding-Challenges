cc97f7a10c1db3da764601233bafe740fbc81607
GiannisTsevrenis <ts3v@outlook.com>
2024-09-30 11:45:37 +0300

    Update 21. Merge Two Sorted Lists.java

--- LeetCode/Recursion/21. Merge Two Sorted Lists.java
+++ LeetCode/Recursion/21. Merge Two Sorted Lists.java
@@ -9,20 +9,21 @@
  * }
  */
 class Solution {
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode();
-        ListNode current = result;
-        while (list1 != null && list2 != null) {
-            if (list1.val >= list2.val) {
-                current.next = list2;
-                list2 = list2.next;
-            } else {
-                current.next = list1;
-                list1 = list1.next;
-            }
-            current = current.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // base cases
+        if (list1 == null) {
+            return list2;
         }
-        current.next = (list1 == null)? list2: list1;
-        return result.next;
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
+
 }
