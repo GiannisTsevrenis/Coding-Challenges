34eecaf5bcea815932aacea26ef7e176c8d073d5
GiannisTsevrenis <ts3v@outlook.com>
2025-07-14 13:19:50 +0200

    Create 1731. The Number of Employees Which Report to Each Employee.sql

---
+++ LeetCode/Databases/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,9 @@
+SELECT
+    e1.employee_id,
+    e1.name,
+    count(e1.employee_id) as reports_count,
+    ROUND(AVG(e2.age)) as average_age
+FROM Employees e1
+JOIN Employees e2 on e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id
+ORDER BY e1.employee_id
