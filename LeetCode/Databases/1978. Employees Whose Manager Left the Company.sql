07ea714fdb27249df7392abf17d222a3cbe720b4
GiannisTsevrenis <ts3v@outlook.com>
2025-07-25 14:42:11 +0200

    Create 1978. Employees Whose Manager Left the Company.sql

---
+++ LeetCode/Databases/1978. Employees Whose Manager Left the Company.sql
@@ -0,0 +1,10 @@
+SELECT
+    employee_id
+FROM
+    Employees
+WHERE
+    salary < 30000 and manager_id not in (
+        select employee_id
+        from Employees
+    )
+ORDER BY employee_id ASC;
