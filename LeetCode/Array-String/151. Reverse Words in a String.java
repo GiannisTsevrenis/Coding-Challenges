cf603b2c85bf12a4f98b2349e15a123284449103
GiannisTsevrenis <ts3v@outlook.com>
2025-12-27 00:01:17 +0100

    Update 151. Reverse Words in a String.java

--- LeetCode/Array-String/151. Reverse Words in a String.java
+++ LeetCode/Array-String/151. Reverse Words in a String.java
@@ -4,4 +4,14 @@ class Solution {
         Collections.reverse(asd);
         return String.join(" ",asd);
     }
+    //
+    //    public String reverseWords(String s) {
+    //     String[] words = s.trim().replaceAll(" +", " ").split(" ");
+    //     for (int i = 0; i < words.length/2; i++) {
+    //         String temp = words[i];
+    //         words[i] = words[words.length - 1 - i];
+    //         words[words.length - 1 - i] = temp;
+    //     }
+    //     return String.join(" ", words);
+    // }
 }
