06c283cddf6dae9c5049ea3513786f4243785c1e
GiannisTsevrenis <ts3v@outlook.com>
2026-02-19 13:01:28 +0100

    Create 735. Asteroid Collision.py

---
+++ LeetCode/Stack/735. Asteroid Collision.py
@@ -0,0 +1,19 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            if asteroid > 0:
+                stack.append(asteroid)
+            else:
+                while stack and stack[-1] > 0:
+                    if abs(asteroid) > abs(stack[-1]):
+                        stack.pop()
+                    elif abs(asteroid) < abs(stack[-1]):
+                        break
+                    else:
+                        stack.pop()
+                        break
+                else:  # comes here when we dont hit a break
+                    stack.append(asteroid)
+
+        return stack
